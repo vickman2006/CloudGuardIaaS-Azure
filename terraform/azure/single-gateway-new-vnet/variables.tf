e6f375cfe6a8be8f829d112317fcf5e9aa752968
Victor Vazquez <vickman2006@users.noreply.github.com>
2025-03-17 13:33:26 -0600

    Update variables.tf

--- terraform/azure/single-gateway-new-vnet/variables.tf
+++ terraform/azure/single-gateway-new-vnet/variables.tf
@@ -1,243 +1,279 @@
-/***********************************************
- *         Provider and Basic Variables        *
- ***********************************************/
-variable "subscription_id" {
-  description = "ID de la suscripción de Azure."
-  type        = string
-}
-
-variable "client_id" {
-  description = "Client ID del Service Principal."
-  type        = string
-}
-
-variable "client_secret" {
-  description = "Client Secret del Service Principal."
-  type        = string
-  sensitive   = true
-}
-
-variable "tenant_id" {
-  description = "Tenant ID de Azure."
+//********************** Basic Configuration Variables **************************//
+variable "single_gateway_name" {
+  description = "Single gateway name"
   type        = string
 }
 
 variable "resource_group_name" {
-  description = "Nombre del grupo de recursos."
+  description = "Azure Resource Group name to build into"
   type        = string
 }
 
 variable "location" {
-  description = "Región de Azure en la que se desplegarán los recursos."
+  description = "The location/region where resource will be created. The full list of Azure regions can be found at https://azure.microsoft.com/regions"
   type        = string
 }
 
+//********************** Virtual Machine Instances Variables **************************//
+variable "source_image_vhd_uri" {
+  type        = string
+  description = "The URI of the blob containing the development image. Please use noCustomUri if you want to use marketplace images."
+  default     = "noCustomUri"
+}
+
+variable "admin_username" {
+  description = "Administrator username of deployed VM. Due to Azure limitations 'notused' name can be used"
+  default     = "notused"
+}
+
 variable "admin_password" {
-  description = "Contraseña de administrador para las VMs."
+  description = "Administrator password of deployed Virtual Machine. The password must meet the complexity requirements of Azure"
   type        = string
   sensitive   = true
 }
 
-variable "installation_type" {
-  description = "Tipo de instalación."
+variable "smart_1_cloud_token" {
+  description = "Smart-1 Cloud Token"
   type        = string
-  default     = "default"
-}
-
-variable "template_name" {
-  description = "Nombre de la plantilla para el despliegue."
-  type        = string
-}
-
-variable "template_version" {
-  description = "Versión de la plantilla."
-  type        = string
-}
-
-variable "allow_upload_download" {
-  description = "Habilitar operaciones de carga/descarga."
-  type        = bool
-  default     = false
-}
-
-variable "vm_size" {
-  description = "Tamaño de la máquina virtual."
-  type        = string
-}
-
-variable "disk_size" {
-  description = "Tamaño del disco en GB."
-  type        = number
-}
-
-variable "os_version" {
-  description = "Versión del sistema operativo."
-  type        = string
-}
-
-variable "vm_os_sku" {
-  description = "SKU del sistema operativo para la VM."
-  type        = string
-}
-
-variable "vm_os_offer" {
-  description = "Oferta del sistema operativo para la VM."
-  type        = string
-}
-
-variable "is_blink" {
-  description = "Indica si se activa la opción Blink."
-  type        = bool
-  default     = false
-}
-
-variable "authentication_type" {
-  description = "Tipo de autenticación (por ejemplo, SSH Public Key o Password)."
-  type        = string
+  sensitive   = true
 }
 
 variable "serial_console_password_hash" {
-  description = "Hash de la contraseña para el acceso al Serial Console."
+  description = "Optional parameter, used to enable serial console connection in case of SSH key as authentication type"
   type        = string
   sensitive   = true
 }
 
 variable "maintenance_mode_password_hash" {
-  description = "Hash de la contraseña para el modo de mantenimiento."
+  description = "Maintenance mode password hash, relevant only for R81.20 and higher versions"
   type        = string
   sensitive   = true
 }
 
-variable "storage_account_additional_ips" {
-  description = "Lista de IPs adicionales permitidas para la cuenta de almacenamiento."
-  type        = list(string)
-  default     = []
-}
-
-/***********************************************
- *         Networking Variables                *
- ***********************************************/
-variable "vnet_name" {
-  description = "Nombre de la Virtual Network (VNet)."
+variable "authentication_type" {
+  description = "Specifies whether a password authentication or SSH Public Key authentication should be used"
   type        = string
 }
 
-variable "address_space" {
-  description = "Espacio de direcciones para la VNet."
-  type        = list(string)
+locals {
+  authentication_type_allowed_values = [
+    "Password",
+    "SSH Public Key"
+  ]
+  validate_authentication_type_value = index(local.authentication_type_allowed_values, var.authentication_type)
 }
 
-variable "frontend_1_subnet_prefix" {
-  description = "Prefijo de la subred para Frontend 1."
+variable "template_name" {
+  description = "Template name. Should be defined according to deployment type(mgmt, ha, vmss, sg)"
+  type        = string
+  default     = "single"
+}
+
+variable "template_version" {
+  description = "Template version. It is recommended to always use the latest template version"
+  type        = string
+  default     = "20230910"
+}
+
+variable "installation_type" {
+  description = "installation type"
+  type        = string
+  default     = "gateway"
+}
+
+locals {
+  installation_type_allowed_values = [
+    "gateway",
+    "standalone"
+  ]
+}
+
+variable "vm_size" {
+  description = "Specifies size of Virtual Machine"
   type        = string
 }
 
-variable "frontend_2_subnet_prefix" {
-  description = "Prefijo de la subred para Frontend 2."
+variable "disk_size" {
+  description = "Storage data disk size (GB). Select a number between 100 and 3995"
   type        = string
 }
 
-variable "backend_subnet_prefix" {
-  description = "Prefijo de la subred para el Backend."
+variable "os_version" {
+  description = "GAIA OS version"
   type        = string
 }
 
-variable "nsg_id" {
-  description = "ID del NSG existente (opcional)."
-  type        = string
-  default     = ""
+locals {
+  os_version_allowed_values = [
+    "R81",
+    "R8110",
+    "R8120",
+    "R82"
+  ]
+  validate_os_version_value = index(local.os_version_allowed_values, var.os_version)
 }
 
-variable "vnet_allocation_method" {
-  description = "Método de asignación de IP para las NICs (por ejemplo, Static o Dynamic)."
+variable "vm_os_sku" {
+  description = "The sku of the image to be deployed."
   type        = string
-  default     = "Dynamic"
 }
 
-/* Valor para el SKU de la IP pública */
-variable "sku" {
-  description = "SKU para el recurso de IP pública (por ejemplo, Basic o Standard)."
+variable "vm_os_offer" {
+  description = "The name of the image offer to be deployed. Choose from: check-point-cg-r81, check-point-cg-r8110, check-point-cg-r8120, check-point-cg-r82"
   type        = string
-  default     = "Standard"
 }
 
-/***********************************************
- *         Storage and Other Variables         *
- ***********************************************/
-variable "add_storage_account_ip_rules" {
-  description = "Habilitar reglas de IP para la cuenta de almacenamiento."
+locals {
+  vm_os_offer_allowed_values = [
+    "check-point-cg-r81",
+    "check-point-cg-r8110",
+    "check-point-cg-r8120",
+    "check-point-cg-r82"
+  ]
+  validate_os_offer_value = index(local.vm_os_offer_allowed_values, var.vm_os_offer)
+}
+
+variable "allow_upload_download" {
+  description = "Automatically download Blade Contracts and other important data. Improve product experience by sending data to Check Point"
   type        = bool
-  default     = false
-}
-
-variable "storage_account_ip_rules" {
-  description = "Lista de reglas de IP permitidas para la cuenta de almacenamiento."
-  type        = list(string)
-  default     = []
-}
-
-variable "source_image_vhd_uri" {
-  description = "URI para la imagen VHD personalizada; usa 'noCustomUri' para imagen predeterminada."
-  type        = string
-  default     = "noCustomUri"
-}
-
-variable "bootstrap_script" {
-  description = "Script de bootstrap para ejecutar en el inicio de la VM."
-  type        = string
-  default     = ""
-}
-
-variable "admin_shell" {
-  description = "Shell a utilizar para el usuario administrador."
-  type        = string
-  default     = "/bin/bash"
-}
-
-variable "sic_key" {
-  description = "SIC key para la configuración."
-  type        = string
-  default     = ""
-}
-
-variable "management_GUI_client_network" {
-  description = "Configuración de red para el cliente de la GUI de administración."
-  type        = string
-  default     = ""
-}
-
-variable "smart_1_cloud_token" {
-  description = "Token para la API de Smart-1 Cloud."
-  type        = string
-  default     = ""
 }
 
 variable "enable_custom_metrics" {
-  description = "Habilitar métricas personalizadas en la configuración de la VM."
-  type        = bool
-  default     = false
-}
-
-variable "delete_os_disk_on_termination" {
-  description = "Indica si se debe eliminar el disco del sistema operativo al terminar la VM."
+  description = "Indicates whether CloudGuard Metrics will be used for Cluster members monitoring."
   type        = bool
   default     = true
 }
 
-variable "storage_os_disk_create_option" {
-  description = "Opción de creación para el disco del sistema operativo."
-  type        = string
-  default     = "FromImage"
+variable "is_blink" {
+  description = "Define if blink image is used for deployment"
+  default     = true
 }
 
-variable "storage_os_disk_caching" {
-  description = "Opción de caché para el disco del sistema operativo."
+variable "admin_shell" {
+  description = "The admin shell to configure on machine for the first time"
   type        = string
-  default     = "ReadWrite"
+  default     = "/etc/cli.sh"
 }
 
-variable "storage_account_type" {
-  description = "Tipo de cuenta de almacenamiento (por ejemplo, Standard_LRS o Premium_LRS)."
+locals {
+  admin_shell_allowed_values = [
+    "/etc/cli.sh",
+    "/bin/bash",
+    "/bin/csh",
+    "/bin/tcsh"
+  ]
+  validate_admin_shell_value = index(local.admin_shell_allowed_values, var.admin_shell)
+}
+
+//********************** Networking Variables **************************//
+variable "vnet_name" {
+  description = "Virtual Network name"
   type        = string
-  default     = "Standard_LRS"
+}
+
+variable "address_space" {
+  description = "The address space that is used by a Virtual Network."
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "frontend_1_subnet_prefix" {
+  description = "Address prefix to be used for network frontend 1 subnet"
+  type        = string
+  default     = "10.0.0.0/24"
+}
+
+variable "frontend_2_subnet_prefix" {
+  description = "Address prefix to be used for network frontend 2 subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "backend_subnet_prefix" {
+  description = "Address prefix to be used for network backend subnet"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "vnet_subnets" {
+  description = "Subnets in vnet"
+  type        = list(string)
+  default     = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "vnet_allocation_method" {
+  description = "IP address allocation method"
+  type        = string
+  default     = "Static"
+}
+
+variable "management_GUI_client_network" {
+  description = "Allowed GUI clients - GUI clients network CIDR"
+  type        = string
+}
+
+variable "nsg_id" {
+  description = "NSG ID - Optional - if empty use default NSG"
+  default     = ""
+}
+
+variable "add_storage_account_ip_rules" {
+  type        = bool
+  default     = false
+  description = "Add Storage Account IP rules that allow access to the Serial Console only for IPs based on their geographic location"
+}
+
+variable "storage_account_additional_ips" {
+  type        = list(string)
+  description = "IPs/CIDRs that are allowed access to the Storage Account"
+  default     = []
+}
+
+locals {
+  regex_valid_network_cidr = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(/(3[0-2]|2[0-9]|1[0-9]|[0-9]))$"
+  regex_management_GUI_client_network = regex(local.regex_valid_network_cidr, var.management_GUI_client_network) == var.management_GUI_client_network ? 0 : "Variable [management_GUI_client_network] must be a valid IPv4 network CIDR."
+}
+
+variable "bootstrap_script" {
+  description = "An optional script to run on the initial boot"
+  default     = ""
+  type        = string
+}
+
+//********************** Credentials **************************//
+variable "tenant_id" {
+  description = "Tenant ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "subscription_id" {
+  description = "Subscription ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Application ID (Client ID)"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Application password."
+  type        = string
+  sensitive   = true
+}
+
+variable "sic_key" {
+  type      = string
+  sensitive = true
+}
+
+resource "null_resource" "sic_key_invalid" {
+  count = length(var.sic_key) >= 12 ? 0 : "SIC key must be at least 12 characters long"
+}
+
+variable "sku" {
+  description = "SKU"
+  type        = string
+  default     = "Standard"
 }
